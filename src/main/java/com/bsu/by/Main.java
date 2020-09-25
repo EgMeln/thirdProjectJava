af403e104f4d2e9f1578ba513cfb612c6fdff9ac
EgMeln <Egmell@yandex.ru>
2020-09-25 13:13:51 +0300

    Initial commit

--- src/main/java/com/bsu/by/Main.java
+++ src/main/java/com/bsu/by/Main.java
@@ -1,4 +1,24 @@
 package com.bsu.by;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter text: ");
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        System.out.println(toLargeLetter(str));
+    }
+
+    static String toLargeLetter(String str) {
+        StringBuilder secondStr = new StringBuilder(str);
+        secondStr.setCharAt(0, Character.toUpperCase(str.charAt(0)));
+        char[] charArray = str.toCharArray();
+        for (int i = 1; i < str.length(); i++) {
+            if (Character.isSpaceChar(str.charAt(i - 1)) | charArray[i - 1] == '.' | charArray[i - 1] == ',') {
+                secondStr.setCharAt(i, Character.toUpperCase(str.charAt(i)));
+            }
+        }
+        return secondStr.toString();
+    }
 }
--- src/test/java/com/bsu/by/MainTest.java
+++ src/test/java/com/bsu/by/MainTest.java
@@ -1,4 +1,13 @@
 package com.bsu.by;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MainTest {
+    @Test
+    void toLargeLetter() {
+        String str = " a b c d fre erq. fwef, deq qf.rw,  fw vf      ewfw";
+        assertEquals(" A B C D Fre Erq. Fwef, Deq Qf.Rw,  Fw Vf      Ewfw", Main.toLargeLetter(str));
+    }
 }
